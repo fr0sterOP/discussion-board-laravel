cf401b3457f64bd048dd9225f5270e51394427ea
shivam varu <shivamvaru0@gmail.com>
2025-12-29 11:43:36 +0530

    Implement role-based access control and admin route

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,9 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::middleware(['auth', 'role:admin'])->get('/admin', function () {
+    return 'Admin Panel';
+});
+
+
+require __DIR__ . '/auth.php';
